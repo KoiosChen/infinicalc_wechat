fb57b5367f43545204fe974ab593945e18a08ef7
koioschen <chjz1226@gmail.com>
2020-07-12 22:45:53 +0800

    默认更改列表

--- app/type_validation.py
+++ app/type_validation.py
@@ -64,5 +64,6 @@ def seckill_type(value):
 
 
 def checkout_sku_type(value):
-    parse_keys = {'id': {'required': True, 'type': str}, 'quantity': {'required': True, 'type': int}}
+    parse_keys = {'id': {'required': True, 'type': str}, 'quantity': {'required': True, 'type': int},
+                  'combo': {'type': dict}}
     return type_base(parse_keys, value)
