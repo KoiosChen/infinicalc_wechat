faeae41128ed71919a724c9a78c28ada882a0ebf
koioschen <chjz1226@gmail.com>
2020-07-18 00:48:36 +0800

    默认更改列表

--- app/swagger.py
+++ app/swagger.py
@@ -7,8 +7,7 @@ return_dict = {'code': fields.String(required=True, description='success | false
                'data': fields.Raw(description='string or json'),
                'message': fields.String(description='成功或者失败的文字信息')}
 
-
 page_parser = reqparse.RequestParser()
-page_parser.add_argument('page', help='true | false, 是否分页')
-page_parser.add_argument('current', type=int, help='当前页, 第一页为1， 必须传大于0的整数')
-page_parser.add_argument('size', type=int, help='当前页行数, 大于0的整数')
+page_parser.add_argument('page', help='true | false, 是否分页', location='args')
+page_parser.add_argument('current', type=int, help='当前页, 第一页为1， 必须传大于0的整数', location='args')
+page_parser.add_argument('size', type=int, help='当前页行数, 大于0的整数', location='args')
