ffdea4938af5f2afcfc934f39097f061cbd6110b
koioschen <chjz1226@gmail.com>
2020-10-12 21:05:29 +0800

    默认更改列表

--- shell.py
+++ shell.py
@@ -2,7 +2,8 @@
 import os
 from app import create_app, db
 from app.models import Users, Roles, user_role, roles_elements, Elements, SKU, Layout, SKULayout, \
-    Classifies, SPU, customer_role, Customers, Promotions, CustomerRoles, Brands, ShopOrders, ShoppingCart
+    Classifies, SPU, customer_role, Customers, Promotions, CustomerRoles, Brands, ShopOrders, ShoppingCart, \
+    NewCustomerAwards, CouponReady
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -15,7 +16,8 @@ migrate = Migrate(app, db)
 
 def make_shell_context():
     return dict(app=app, db=db, Users=Users, Customers=Customers, ShoppingCart=ShoppingCart,
-                CustomerRoles=CustomerRoles, Brands=Brands, SKU=SKU, ShopOrders=ShopOrders, Promotions=Promotions)
+                CustomerRoles=CustomerRoles, Brands=Brands, SKU=SKU, ShopOrders=ShopOrders, Promotions=Promotions,
+                NewCustomerAwards=NewCustomerAwards, CouponReady=CouponReady)
 
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
