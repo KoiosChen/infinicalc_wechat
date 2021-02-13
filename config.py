1a00b0d865f67cef762286a3a004568cdb21d730
koioschen <chjz1226@gmail.com>
2021-02-13 12:10:18 +0800

    默认更改列表

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ class TestingConfig(Config):
 class ProductionConfig(Config):
     DB_USERNAME = os.environ.get('DATABASE_USERNAME') or 'peter'
     DB_PASSWORD = os.environ.get('DATABASE_PASSWORD') or 'Gwbnsh@408'
-    DB_HOST = os.environ.get('DATABASE_HOST') or '42.194.141.250'
+    DB_HOST = os.environ.get('DATABASE_HOST') or '127.0.0.1'
     DB_DATABASE = os.environ.get('DATABASE_DATABASE') or 'shengzhuan'
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + DB_USERNAME + ':' + DB_PASSWORD + '@' + DB_HOST + '/' + DB_DATABASE
 
