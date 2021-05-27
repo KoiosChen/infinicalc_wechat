7b2885bdbefc58a131d968d27b4ded44d55e69a2
koioschen <chjz1226@gmail.com>
2021-05-27 23:21:37 +0800

    默认更改列表

--- app/wechat/update_order.py
+++ app/wechat/update_order.py
@@ -66,6 +66,7 @@ def update_order(data):
                 customer.first_order_id = order.id
 
             # 2021-0509 这个if用于目前加盟商直营团购付款成功之后升级用户等级
+            logger.debug(order.upgrade_level + customer.level)
             if order.upgrade_level and customer.level < order.upgrade_level:
                 customer.level = order.upgrade_level
 
