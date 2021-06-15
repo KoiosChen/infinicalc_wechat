980ed858b57fb4a47c40fb540891b755d8b5289b
koioschen <chjz1226@gmail.com>
2021-06-15 23:11:31 +0800

    默认更改列表

--- app/business_units/business_untis_api.py
+++ app/business_units/business_untis_api.py
@@ -147,6 +147,7 @@ class BusinessUnitsAPI(Resource):
             addr_result = lbs_get_by_coordinate(args['latitude'], args['longitude'], detail=True)
             in_scope = False
             logger.debug(addr_result)
+            addr_result = addr_result['data']
             for scope in franchisee_scopes:
                 logger.debug(scope)
                 if addr_result['province'] == scope.province and addr_result['city'] == scope.city and addr_result['district'] == scope.district:
