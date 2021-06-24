6515315ad361fa8eafaf8e24b1f9787d27fb30a0
koioschen <chjz1226@gmail.com>
2021-06-24 19:54:56 +0800

    默认更改列表

--- app/business_units/business_untis_api.py
+++ app/business_units/business_untis_api.py
@@ -398,7 +398,8 @@ class BUEmployeesApi(Resource):
             bu_id = kwargs['current_user'].business_unit_employee.business_unit_id
 
         # 店铺经理和店长在一个店铺中唯一
-        role_name = "BU_WAITER"
+        role_name = args.get('job_desc')
+        logger.debug(role_name)
         job_id = CustomerRoles.query.filter_by(name=role_name).first().id
         if role_name in ("BU_OPERATOR", "BU_MANAGER"):
             tmp_obj = BusinessUnitEmployees.query.filter(BusinessUnitEmployees.business_unit_id.__eq__(bu_id),
